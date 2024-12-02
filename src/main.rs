c8a069db2464fa23b970aae0d15604766ce6ee3f
Jeff Manning <info@jeffmanning.ca>
2024-12-02 15:29:26 -0500

    day 2

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fs::read_to_string;
 
 fn main() {
     day1();
+    day2();
 }
 
 fn day1() {
@@ -31,3 +32,83 @@ fn day1() {
     println!("Day 1 P1: {}",total);
     println!("Day 1 P2: {}",p2_total);
 }
+
+fn day2() {
+    let mut total_safe: i32 = 0;
+    let mut total_safe_p2: i32 = 0;
+    for line in read_to_string("inputs/day2.txt").unwrap().lines() {
+        let res: Vec<&str> = line.split(" ").collect();
+        let v1 = check_line_day_2(&res);
+        let mut v2 = false;
+        if !v1 {
+            for (index, _) in res.iter().enumerate() {
+                let mut new_res = res.clone();
+                new_res.remove(index);
+                if check_line_day_2(&new_res) {
+                    v2 = true;
+                    break;
+                }
+            }
+        }
+
+        if v1 {
+            total_safe +=1;
+            total_safe_p2 +=1;
+        }
+        if v2 {
+            total_safe_p2 +=1;
+        }
+    }
+
+    println!("Day 2 P1: {}", total_safe);
+    println!("Day 2 P2: {}", total_safe_p2);
+}
+
+fn check_day_2(previous: i32, current:i32, increasing: bool) -> bool{
+    if previous > current && increasing {
+        return false
+    } else if previous < current && !increasing {
+        return false
+    } else if previous == current {
+        return false
+    }
+
+    if (previous - current).abs() > 3 {
+        return false
+    }
+
+    true
+}
+
+fn check_line_day_2(res: &Vec<&str>) -> bool {
+    let mut previous_number = 0;
+    let mut increasing: bool = false;
+    for (index, num) in res.iter().enumerate() {
+        let num = num.parse::<i32>().unwrap();
+        if index == 0 {
+            previous_number = num;
+        } else if index == 1 {
+            if previous_number > num {
+                increasing = false;
+            } else if previous_number < num {
+                increasing = true;
+            } else {
+                return false;
+            }
+
+            if (previous_number - num).abs() > 3 {
+                return false;
+            }
+        } else {
+            if !check_day_2(previous_number,num,increasing) {
+                return false;
+            }
+        }
+
+        previous_number = num;
+    }
+
+    true
+}
+
+
